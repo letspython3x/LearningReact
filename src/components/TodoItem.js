240d5ef4731e7323048d017d57df88375f15c6a1
Abhishake Gupta <abhi6196@mzp9bjmd6m.lan>
2020-05-10 19:07:16 +0530

    Add todoitem js file

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TodoItem() {
+    return (
+        <div class="todo-item">
+            <input type="checkbox" />
+            <p>Placeholder Here</p>
+        </div>
+    )
+}
+
+export default TodoItem
