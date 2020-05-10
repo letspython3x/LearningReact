64523584b26a7e185d506e8b40d283fee7cb2bac
Abhishake Gupta <abhi6196@mzp9bjmd6m.lan>
2020-05-10 15:36:43 +0530

    Update not required code down the file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>)
+}
+
+export default App;
+
+
 // function App() {
 //   return (
 //     <div className="App">
@@ -26,14 +38,3 @@ import Footer from './components/Footer'
 //     </div>
 //   );
 // }
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>)
-}
-
-export default App;
