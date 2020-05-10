18e689d13346cead30840f71f0f047ffd3a199b4
Abhishake Gupta <abhi6196@mzp9bjmd6m.lan>
2020-05-10 19:30:53 +0530

    Delete Extra Files

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import ContactCard from './components/ContactCard'
 
 function App() {
-  const date = new Date()
-  const hours = date.getHours()
-  let timeOfDay
-
-  if (hours < 12) {
-    timeOfDay = "morning"
-  }
-  else if (hours > 12 && hours < 17) {
-    timeOfDay = "evening"
-  }
-  else {
-    timeOfDay = "night"
-  }
-
   return (
-    <div>
-      <Header />
-      <h1>Good {timeOfDay}</h1>
-      <MainContent />
-      <Footer />
-    </div>)
+    <div className="contacts">
+      <ContactCard contact={{ name: "Keshav Kansal", phone: "99889898", imgUrl: "https://i.imgur.com/8pTwPlXb.jpg", email: "abhi1@abhi.com" }} />
+      <ContactCard contact={{ name: "Mukta Gupta", phone: "99889898", imgUrl: "https://i.imgur.com/JXetxQhb.jpg", email: "abhi2@abhi.com" }} />
+      <ContactCard contact={{ name: "Siddhi Gupta", phone: "99889898", imgUrl: "https://i.imgur.com/qw09M6Yb.jpg", email: "abhi3@abhi.com" }} />
+      <ContactCard contact={{ name: "Akshay khanna", phone: "99889898", imgUrl: "https://i.imgur.com/kkDYbHZb.jpg", email: "abhi4@abhi.com" }} />
+    </div>
+  )
 }
 
-export default App;
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+export default App
