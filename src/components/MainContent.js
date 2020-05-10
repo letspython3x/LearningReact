c7b257c707005d7dfccd2ff1c0f68b93ccd2b870
Abhishake Gupta <abhi6196@mzp9bjmd6m.lan>
2020-05-10 17:49:42 +0530

    Add Inline styling

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,11 @@
 import React from "react"
 
+// Using Inline styling
+
 function MainContent() {
     return (
-        <main className="main-content">My content is here...</main>)
+        <main className="main-content">My content is here...
+            <p style={{ color: "#333", backgroundColor: "#777" }}>This is another line of main content</p>
+        </main>)
 }
 export default MainContent
