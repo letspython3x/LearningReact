055d875d988dc66f787096f7b9e640385ffef5a2
Abhishake Gupta <abhi6196@mzp9bjmd6m.lan>
2020-05-10 17:43:01 +0530

    Mixture of html & Javascript in React component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,24 @@ import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 
 function App() {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  }
+  else if (hours > 12 && hours < 17) {
+    timeOfDay = "evening"
+  }
+  else {
+    timeOfDay = "night"
+  }
+
   return (
     <div>
       <Header />
+      <h1>Good {timeOfDay}</h1>
       <MainContent />
       <Footer />
     </div>)
