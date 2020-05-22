1c9a08047681523079f29a9158e593f69337aa69
Abhishake Gupta <letspython3.x@gmail.com>
2020-05-22 17:52:50 +0530

    Add Jokes data from a separate file

--- src/App.js
+++ src/App.js
@@ -2,30 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Joke from './components/Joke'
+import jokesData from './components/jokesData'
 
 function App() {
+  // const jokes  = jokesData.map(function(joke){
+  //   return <Joke question={joke.question} punchLine={joke.punchLine} />
+  // })
+ 
+  const jokes = jokesData.map(joke => <Joke key ={joke.id} question={joke.question} punchLine={joke.punchLine}/>)
+
 
   return (
     <div>
-      <Joke
-        punchLine="Need a one Liner Joke.."
-      />
-
-      <Joke
-        question="How can you drop a raw egg onto a concrete floor without cracking it?"
-        punchLine="Concrete floors are very hard to crack!"
-      />
-      <Joke
-        question="Approximately how many birthdays does the average Indian woman have?"
-        punchLine="Just one. All the others are anniversaries."
-      />
-      <Joke
-        question="Q. How can a man go eight days without sleep?"
-        punchLine="A. He sleeps at night."
-      />
-      <Joke
-        question="What often falls but never gets hurt?"
-        punchLine="Rain" />
+      {jokes}
     </div>
   )
 }
