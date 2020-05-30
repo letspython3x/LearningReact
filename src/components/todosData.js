595443bff52f2afe2527997dbfae1de21795b178
Abhishake Gupta <letspython3.x@gmail.com>
2020-05-30 12:13:28 +0530

    Add todosData

---
+++ src/components/todosData.js
@@ -0,0 +1,121 @@
+const todosData = [
+    {
+        id: 1,
+        completed: true,
+        text: "Intro to JSX"
+    },
+    {
+        id: 1,
+        completed: true,
+        text: "React Functional Components"
+    },
+    {
+        id: 1,
+        completed: true,
+        text: "Move Components to a separate file"
+    },
+    {
+        id: 1,
+        completed: true,
+        text: "Parent Child Components"
+    },
+
+    {
+        id: 1,
+        completed: true,
+        text: "JSX to Javascript and back"
+    },
+    {
+        id: 1,
+        completed: true,
+        text: "Styling react with CSS Classes"
+    },
+    {
+        id: 1,
+        completed: true,
+        text: "Styling react with Inline CSS Classes"
+    },
+    {
+        id: 1,
+        completed: true,
+        text: "React Props & Styling"
+    },
+
+    {
+        id: 1,
+        completed: true,
+        text: "React Class Based Components"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "React State"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "Handling Events"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "setState: Changing the state"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "React Lifecycle methods"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "React Conditional Render"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "React Fetch Data from API"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "React Forms"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "React Container & Component Arch"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "React Meme Generator"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "Writing Modern React Apps"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "Hooks Intro"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "React useState()"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "React useEffect()"
+    },
+    {
+        id: 1,
+        completed: false,
+        text: "React Project Ideas for Practicing"
+    },
+]
+
+export default todosData
