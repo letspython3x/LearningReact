1d7bd8f125b82e7f650d9fc8ab9b18e35cdf7ce5
abhishake <abhishake.gupta@rackspace.com>
2021-04-20 16:25:02 +0530

    Add useState Documentation

--- src/functionalComponents/StateHookWithObject.js
+++ src/functionalComponents/StateHookWithObject.js
@@ -1,3 +1,19 @@
+/*
+****************************************************************************
+- useState React Hook returns an array of 2 variables.
+- 1st the current value of the state
+- 2nd setter for the state variable
+
+****************************************************************************
+- State is always an object in class bases components but with useState,
+  state can be of any datatype viz. bool, int, string, array, object.
+- When Dealing with objects/arrays, always make sure to spread your state
+  variable and then call the setter function.
+****************************************************************************
+
+*/
+
+
 import React, {useState} from "react";
 
 export default function StateHookWithObject() {
